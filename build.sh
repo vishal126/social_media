7a65e5b6ad7c8e4cede035baf59bb125e222d2c0
Vishal Menariya <63151018+vishal126@users.noreply.github.com>
2025-03-29 12:27:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 docker build -t $JOB_NAME:$BUILD_ID .
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID
+docker tag $JOB_NAME:$BUILD_ID vishal161205/$JOB_NAME:$BUILD_ID
 
-docker tag $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker tag $JOB_NAME:$BUILD_ID vishal161205/$JOB_NAME:latest
 
-docker push jacksneel/$JOB_NAME:$BUILD_ID
+docker push vishal161205/$JOB_NAME:$BUILD_ID
 
-docker push jacksneel/$JOB_NAME:latest
+docker push vishal161205/$JOB_NAME:latest
 
-docker rmi -f $JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
+docker rmi -f $JOB_NAME:$BUILD_ID vishal161205/$JOB_NAME:$BUILD_ID jacksneel/$JOB_NAME:latest
